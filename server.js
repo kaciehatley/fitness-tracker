751991b913cb8357550bc0b43941d402f176d6f1
Kacie Hatley <kaciehatley@gmail.com>
2020-02-22 20:05:44 -0500

    added comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Requiring npm packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -7,21 +8,22 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+// Mongoose connection
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
 })
 
+// Requiring routes
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
+// Listening...
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
